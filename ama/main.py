9dd2590c2b34e66965a358ff565e3d9051ff5042
Albert Sadowski <asadowski@mailbox.org>
2023-05-11 23:31:08 +0100

    add context endpoint

--- ama/main.py
+++ ama/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from ama.db import SessionLocal
 from ama.repository import get_text_chunks
-from ama.schemas import TextChunk
+from ama.schemas import Status, TextChunk, URL
 
 
 app = FastAPI()
@@ -17,10 +17,19 @@ def get_db():
         db.close()
 
 
-@app.get("/api/v1/text_chunk", response_model=list[TextChunk])
+@app.get("/api/v1/context", response_model=list[TextChunk])
 def search_text_chunks(offset: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return get_text_chunks(
         db=db,
         offset=offset,
         limit=limit,
     )
+
+
+@app.put("/api/v1/context", response_model=Status)
+def add_url_to_context(url: URL, db: Session = Depends(get_db)):
+    # TODO fetch URL
+    # TODO HTML to text
+    # TODO split text into chunks
+    # TODO for each chunk, compute and persist embeddings
+    return Status(success=True)
--- ama/schemas.py
+++ ama/schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
 
 
+class URL(BaseModel):
+    url: str
+
+
+class Status(BaseModel):
+    success: bool
+
+
 class TextChunk(BaseModel):
     id: int
     text: str
