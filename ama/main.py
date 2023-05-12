f1342fbce2f50c7aa2d6e23c1a26c771d6feb513
Albert Sadowski <asadowski@mailbox.org>
2023-05-12 07:55:03 +0100

    create embeddings

--- ama/main.py
+++ ama/main.py
@@ -1,16 +1,20 @@
+import os
 from urllib.request import urlopen
 
+import openai
 from bs4 import BeautifulSoup
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
 from ama.db import SessionLocal
-from ama.repository import get_text_chunks
+from ama.models import TextChunk
+from ama.repository import create_text_chunk, get_text_chunks
 from ama.schemas import Status, TextChunk, URL
 
 
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
+OPENAI_EMBEDDING_ENGINE = "text-embedding-ada-002"
 
 
 app = FastAPI()
@@ -48,6 +52,13 @@ def iter_chunks(text: str):
         offset += CHUNK_SIZE
 
 
+def get_embedding(text: str):
+    return openai.Embedding.create(
+        input=[text.replace("\n", " ")],
+        engine=OPENAI_EMBEDDING_ENGINE,
+    )["data"][0]["embedding"]
+
+
 @app.get("/api/v1/context", response_model=list[TextChunk])
 def search_text_chunks(offset: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return get_text_chunks(
@@ -62,7 +73,11 @@ def add_url_to_context(url: URL, db: Session = Depends(get_db)):
     html = urlopen(url.url).read()
     text = get_text(html)
 
-    print("\n\n".join(iter_chunks(text)))
+    for chunk in iter_chunks(text):
+        embedding = get_embedding(chunk)
+        create_text_chunk(db, chunk, embedding)
+
+    db.commit()
 
     # TODO for each chunk, compute and persist embeddings
     return Status(success=True)
--- ama/repository.py
+++ ama/repository.py
@@ -3,5 +3,14 @@ from sqlalchemy.orm import Session
 from ama.models import TextChunk
 
 
+def create_text_chunk(db: Session, text: str, embedding: list[float]) -> TextChunk:
+    record = TextChunk(
+        text=text,
+        embedding=embedding,
+    )
+    db.add(record)
+    return record
+
+
 def get_text_chunks(db: Session, offset: int, limit: int):
     return db.query(TextChunk).offset(offset).limit(limit).all()
