703feb16e01cdf4d8b79a9cba8d11b82f2ef8ce2
Albert Sadowski <asadowski@mailbox.org>
2023-06-10 16:28:23 +0100

    use mapped_column

--- ask_me_anything/models.py
+++ ask_me_anything/models.py
@@ -1,6 +1,6 @@
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from ask_me_anything.db import Base
 
@@ -8,21 +8,21 @@ from ask_me_anything.db import Base
 class TextChunk(Base):
     __tablename__ = "text_chunk"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    source_url = Column(String)
-    text = Column(String)
-    embedding = mapped_column(Vector(1536))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    source_url: Mapped[str] = mapped_column(String)
+    text: Mapped[str] = mapped_column(String)
+    embedding: Mapped[list[float]] = mapped_column(Vector(1536))
 
 
 class Chat(Base):
     __tablename__ = "chat"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
 
 class Message(Base):
     __tablename__ = "message"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    chat_id = ForeignKey(Chat.id)
-    text = Column(String)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    chat_id: Mapped[int] = mapped_column(ForeignKey(Chat.id))
+    text: Mapped[str] = mapped_column(String)
