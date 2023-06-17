eda7682f38de238f2c82458920c783008d68b170
Albert Sadowski <asadowski@mailbox.org>
2023-06-17 07:21:55 +0100

    add chat prompt

--- ask_me_anything/main.py
+++ ask_me_anything/main.py
@@ -47,6 +47,40 @@ question_prompt = PromptTemplate(
 )
 question_chain = LLMChain(llm=llm, prompt=question_prompt)
 
+chat_prompt = PromptTemplate(
+    input_variables=["context", "history"],
+    template="""
+Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks,
+from answering simple questions to providing in-depth explanations and discussions
+on a wide range of topics. As a language model, Assistant is able to generate
+human-like text based on the input it receives, allowing it to engage in
+natural-sounding conversations and provide responses that are coherent and relevant
+to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are
+constantly evolving. It is able to process and understand large amounts of text,
+and can use this knowledge to provide accurate and informative responses to a wide
+range of questions. Additionally, Assistant is able to generate its own text based
+on the input it receives, allowing it to engage in discussions and provide
+explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks
+and provide valuable insights and information on a wide range of topics. Whether
+you need help with a specific question or just want to have a conversation about
+a particular topic, Assistant is here to assist.
+
+Assistant may use an additional context potentially relevant for the conversation:
+{context}
+End of context.
+
+Conversation with Assistant:
+{history}
+Assistant:""".strip(),
+)
+chat_chain = LLMChain(llm=llm, prompt=chat_prompt)
+
 
 def get_db():
     db = SessionLocal()
@@ -136,7 +170,27 @@ def submit_message(
 ):
     chat = get_chat(db, chat_id)
     create_message(db, chat_id, Author.HUMAN, message)
+    db.commit()
 
+    messages = get_messages(db, chat_id)
+    history = "\n".join([
+        (
+            f"Human: {m.text}"
+            if m.author == Author.HUMAN
+            else f"Assistant: {m.text}"
+        )
+        for m in messages
+    ][-10:])
+
+    embedding = get_embedding(history)
+    ns = find_neighbours(db, embedding, 1)
+    context = "\n\n".join(n.text for n in ns)
+
+    assistant_message = chat_chain.run(
+        context=context,
+        history=history,
+    )
+    create_message(db, chat_id, Author.BOT, assistant_message)
     db.commit()
 
     return templates.TemplateResponse("chat.html", {
